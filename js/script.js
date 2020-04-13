e2eecd598688dcefa4d44ab9d861b0aaba3c2a06
saraclaeys <sara.claeys01@gmail.com>
2020-04-13 11:15:11 +0200

    script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 let color = ["#84134A", "#FFDD02", "#141518", "#07FB70", "#041E3F", "#F133BE"];
+let i = 0;
+document.querySelector("button").addEventListener("click", function () {
+    i = i < color.length ? ++i : 0;
+    document.querySelector("body").style.background = color[i];
+})
