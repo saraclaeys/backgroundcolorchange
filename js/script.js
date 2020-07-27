9196d1724c431cab2f104621c786df6b1618fe39
saraclaeys <sara.claeys01@gmail.com>
2020-07-27 13:41:50 +0200

    semicolon

--- js/script.js
+++ js/script.js
@@ -3,4 +3,4 @@ let i = 0;
 document.querySelector("button").addEventListener("click", function () {
     i = i < color.length ? ++i : 0;
     document.querySelector("body").style.background = color[i];
-})
+});
